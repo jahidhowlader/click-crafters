895559983678ccafcd70b24585c8f608304afb16
Jahid Howlader <jahidhowlader.info@gmail.com>
2023-06-07 13:54:36 +0600

    added Footer

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,9 +1,18 @@
+import Lottie from "lottie-react";
 import Error404 from './Error-404.json'
+import { Link } from "react-router-dom";
 
 const Error = () => {
     return (
-        <div>
-            
+        <div className="grid grid-cols-5 h-[100vh]">
+            <div>
+                <Link to="/">
+                <button className="bg-blue py-1 lg:py-3 px-3 lg:px-8 lg:text-xl text-white m-12">Home</button>
+
+                </Link>
+            </div>
+            <Lottie animationData={Error404} loop={true} className="col-span-3"></Lottie>
+
         </div>
     );
 };
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,14 +3,19 @@ import {
 } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
 import Home from "../pages/home/Home";
+import Error from "../pages/error/Error";
 
 const router = createBrowserRouter([
+    {
+        path: "*",
+        element: <Error></Error>
+    },
     {
         path: "/",
         element: <MainLayout></MainLayout>,
         children: [
             {
-                path: '/',
+                path: "/",
                 element: <Home></Home>
             }
         ]
